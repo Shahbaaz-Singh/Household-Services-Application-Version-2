d89301538777039b64f1a9e145b11a5151678b60
Shahbaaz Singh <ShahbaazSingh8084@gmail.com>
2025-05-12 16:40:28 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'Shahbaaz-Yeager')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'secret-key')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
